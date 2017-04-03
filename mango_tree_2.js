19d84519a3bb30409e26e9eace67401be8d35225
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2017-04-03 12:11:00 +0800

    release 1 done

--- mango_tree_2.js
+++ mango_tree_2.js
@@ -96,6 +96,7 @@ function getRandomNumber() {
 }
 
 
+/*
 var tree = new AppleTree();
 console.log(`The tree is alive! :smile:`);
 
@@ -107,58 +108,100 @@ do {
 } while (tree.healthy != false)
 
 console.log('The tree has met its end. :sad:');
+*/
 
-
-
+// ========================================================================
 // Release 1
 class FruitTree {
   constructor(args) {
-    this.age = 0;
-    this.height = 0; // getRandomNumber();
-    this.fruits = [];
-    this.harvested = "";
-    this.healthy = true;
-
+    this._age = 0;
+    this._height = 0; // getRandomNumber();
+    this._fruits = [];
+    this._harvested = "";
+    this._health = true;
+    this._max_age = 30;
     // params
     // height_increment, max_growth_age, min_growth_age, max_fruits_per_year, fruit_name
 
-    this.height_increment = 30 ; // in cm
+    this._height_increment = 30; // in cm
     if(args.hasOwnProperty('height_increment'))
-      this.height_increment = args['height_increment'];
+      this._height_increment = args['height_increment'];
 
-    this.max_growth_age = 20;
+    this._max_growth_age = 20;
     if(args.hasOwnProperty('max_growth_age'))
-      this.max_growth_age = args['max_growth_age'];
+      this._max_growth_age = args['max_growth_age'];
 
-    this.min_growth_age = 3;
+    this._min_growth_age = 3;
     if(args.hasOwnProperty('min_growth_age'))
-      this.min_growth_age = args['min_growth_age'];
+      this._min_growth_age = args['min_growth_age'];
 
-    this.max_fruits_per_year = 100;
+    this._max_fruits_per_year = 100;
     if(args.hasOwnProperty('max_fruits_per_year'))
-      this.max_fruits_per_year = args['max_fruits_per_year'];
+      this._max_fruits_per_year = args['max_fruits_per_year'];
 
-    this.fruit_name = "";
+    this._fruit_name = "Unknown";
     if(args.hasOwnProperty('fruit_name'))
-      this.fruit_name = args['fruit_name'];
+      this._fruit_name = args['fruit_name'];
 
     // this.max_height = 250;
     // this.max_fruits = getRandomNumber() * 4;
 
-    this.countGood = 0;
-    this.countBad = 0;
+    this._countGood = 0;
+    this._countBad = 0;
+
+  }
+
+  get age() {
+    return this._age;
+  }
+
+  get height() {
+    return this._height;
+  }
+
+  get fruits() {
+    return this._fruits;
+  }
+
+  get health() {
+    return this._health;
   }
 
   grow() {
+    if (this._health) {
+      if(this._age < this._max_age)
+        this._age++;
 
+      if(this._age >= this._max_age)
+        this._health = false;
+
+      // height increase if age is between min and max growth age
+      if(this._age >= this._min_growth_age && this._age <= this._max_growth_age)
+        this._height += this._height_increment;
+
+    }
   }
 
   produceFruits() {
+    let random = getRandomNumber();
+    let totalProduced = random * 12 > this._max_fruits_per_year ? this._max_fruits_per_year : random * 12;
 
+    for (let i = 0 ; i < totalProduced ; i++) {
+      let fruit = new Fruit();
+      // count the mango qualities
+      fruit._quality == "good" ? this._countGood++ : this._countBad++;
+      // store the produced mangoes in fruits array
+      this._fruits.push(fruit);
+    }
   }
 
   harvest() {
+    this.harvested = `${this._fruits.length} (${this._countGood} good, ${this._countBad} bad)`;
 
+    // after harvest, empty the fruits array, and reset count
+    this._fruits = [];
+    this._countBad = 0;
+    this._countGood = 0;
   }
 
 
@@ -166,8 +209,8 @@ class FruitTree {
 
 class Fruit {
   constructor () {
-    this.diameter = getRandomNumber();
-    this.quality = (getRandomNumber()%2 == 0) ? "good" : "bad";
+    this._diameter = getRandomNumber();
+    this._quality = (getRandomNumber()%2 == 0) ? "good" : "bad";
   }
 }
 
@@ -185,10 +228,50 @@ class Mango extends Fruit {
 }
 
 
+var tree = new MangoTree({fruit_name: "Mango"});
+console.log(`The ${tree._fruit_name} tree is alive! :smile:`);
+
+do {
+  tree.grow();
+  tree.produceFruits();
+  tree.harvest();
+  console.log(`[Year ${tree.age} Report] Height = ${tree.height} cm | Fruits harvested = ${tree.harvested}`)
+} while (tree.health != false)
+
+console.log(`The ${tree._fruit_name} tree has met its end. :sad:`);
 
 
+class PearTree extends FruitTree {
+  constructor (args) {
+    super(args);
+  }
+}
+
+class Pear extends Fruit {
+  constructor() {
+    super();
+  }
+}
+
+var pearTree = new PearTree({fruit_name: "Pear", min_growth_age: 10 , height_increment: 50});
+console.log(`The ${pearTree._fruit_name} tree is alive! :smile:`);
+
+do {
+  pearTree.grow();
+  pearTree.produceFruits();
+  pearTree.harvest();
+  console.log(`[Year ${pearTree.age} Report] Height = ${pearTree.height} cm | Fruits harvested = ${pearTree.harvested}`)
+} while (pearTree.health != false)
+
+console.log(`The ${pearTree._fruit_name} tree has met its end. :sad:`);
 
 
 
 // Release 2
 class TreeGrove {}
+
+
+
+
+
+//
